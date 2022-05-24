0324a2c4c5803765507c626f31248aa6fde63292
saltydog1980 <mikeelambert99@gmail.com>
2022-05-24 17:13:23 -0400

    final code

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,19 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+    if (parseInt(arrayToSearchThrough.map((element, i) => element === valueToFind ? i : '').join('')) >= 0) {
+        return Number(arrayToSearchThrough.map((element, i) => element === valueToFind ? i : '').join(''))
+    } else {
+        return undefined
+    }  
 };
 
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+    let ansArr = [];
+  for (let i = 0; i < arrayToSearchThrough.length; i++) {
+      if (arrayToSearchThrough[i] === valueToFind) {
+          ansArr.push(i);
+      }
+  }
+   return ansArr
+};
